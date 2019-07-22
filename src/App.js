4dab46d330af4e519e935a94de048b217074f920
DickensJuma <dickensjuma13@gmail.com>
2019-07-22 15:50:04 +0300

    practising passing props and state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import SingleTodo from './SingleTodo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+/**
+* @author
+* @class App
+**/
+
+class App extends Component {
+  constructor(props){
+    super(props);
+  
+ this.state = {
+   todos:[],
+   currenttodo:''
+ };
 }
+onInputChange = e => {
+  this.setState({
+    currenttodo:e.target.value
+  })
+}
+onClick = () =>{
+  let todocopy = this.state.todos.slice();
+  todocopy.push(this.state.currenttodo);
+  this.setState({
+    todos:todocopy, currenttodo:''
+  });
+}
+deleteTodo = (i) =>{
+  let todocopy = this.state.todos.slice();
+ todocopy.splice(i, 1);
+ this.setState({todos: todocopy})
+  
+}
+
+ render() {
+   let BulletTodos = this.state.todos.map((e,i) => {
+   return(
+     <SingleTodo todo={e} delete={() => this.deleteTodo(i)}/>
+   );
+   });
+   
+  return(
+   <div>
+    <input placeholder='add todo' value={this.state.currenttodo} onChange={this.onInputChange} />
+    <button onClick={this.onClick}>Add</button>
+    {this.state.todos.length === 0 ? ' no todo' : <ul>{BulletTodos}</ul>}
+   </div>
+    );
+   }
+  }
+
+
+
 
 export default App;
